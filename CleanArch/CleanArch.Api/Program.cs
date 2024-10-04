cc2932b5273b470ab482b6f0f4ccfc319fcc3f03
Amresh <amresh.kumar@photon.in>
2024-10-04 20:18:28 +0530

    Added Swagger

--- CleanArch/CleanArch.Api/Program.cs
+++ CleanArch/CleanArch.Api/Program.cs
@@ -1,3 +1,11 @@
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.Swagger;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<UniversityDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UniversityDBConnection"));
+});
+
+builder.Services.AddSwaggerGen(cfg =>
+{
+    cfg.SwaggerDoc("v1", new OpenApiInfo { Title = "University Api", Version = "v1" });
+});
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+RegisterServices(builder.Services);
 
 var app = builder.Build();
 
@@ -23,3 +45,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void RegisterServices(IServiceCollection services)
+{
+    DependencyContainer.RegisterServices(services);
+}
