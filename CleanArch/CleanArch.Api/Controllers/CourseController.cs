d2afcf81b76831a6f70717dee9e489ec76a5274a
Amresh <amresh.kumar@photon.in>
2024-10-04 19:25:08 +0530

    Added API Project and Renamed Controller

---
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+        
+    }
+}
